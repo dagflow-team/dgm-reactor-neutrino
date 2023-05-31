146c1069aee27fc104304bb4bc8112a7af2cb68b
Tsegelnik Nikita <tsegelnik@jinr.ru>
2023-05-31 15:25:05 +0300

    fix a lot of notes in NueSurvivalProbability

--- reactornueosc/NueSurvivalProbability.py
+++ reactornueosc/NueSurvivalProbability.py
@@ -1,11 +1,11 @@
 from dagflow.nodes import FunctionNode
 from dagflow.typefunctions import (
-    check_input_dimension,
     check_input_shape,
+    check_input_subtype,
     copy_from_input_to_output,
 )
 from numba import float64, njit, void
-from numpy import empty, float_, sin, sqrt
+from numpy import empty, float_, integer, sin, sqrt
 from numpy.typing import NDArray
 
 
@@ -13,6 +13,8 @@ from numpy.typing import NDArray
     void(
         float64[:],
         float64[:],
+        float64[:],
+        float64,
         float64,
         float64,
         float64,
@@ -23,30 +25,35 @@ from numpy.typing import NDArray
 )
 def _osc_prob(
     out: NDArray[float_],
-    L4E: NDArray[float_],
+    buffer: NDArray[float_],
+    E: NDArray[float_],
+    L: float,
     sinSq2Theta12: float,
     sinSq2Theta13: float,
     DeltaMSq21: float,
     DeltaMSq32: float,
     alpha: float,
 ) -> None:
+    _DeltaMSq32 = alpha * DeltaMSq32  # Δm²₃₂ = α*|Δm²₃₂|
     _DeltaMSq31 = alpha * DeltaMSq32 + DeltaMSq21  # Δm²₃₁ = α*|Δm²₃₂| + Δm²₂₁
-    _DeltaMSq32 = (
-        alpha * abs(_DeltaMSq31) - DeltaMSq21
-    )  # Δm²₃₂ = α*|Δm²₃₁| - Δm²₂₁
     _sinSqTheta12 = 0.5 * (1 - sqrt(1 - sinSq2Theta12))  # sin²2θ₁₂
     _cosSqTheta12 = 1.0 - _sinSqTheta12  # cos²2θ₁₂
     _cosQuTheta13 = (0.5 * (1 - sqrt(1 - sinSq2Theta13))) ** 2  # cos^4 θ₁₃
 
-    out[:] = (
-        1
-        - sinSq2Theta13
-        * (
-            _sinSqTheta12 * sin(_DeltaMSq32 * L4E[:]) ** 2
-            + _cosSqTheta12 * sin(_DeltaMSq31 * L4E[:]) ** 2
+    for i in range(len(E)):
+        buffer[i] = L / 4.0 / E[i]  # common factor
+
+    for i in range(len(out)):
+        tmp = buffer[i]
+        out[i] = (
+            1
+            - sinSq2Theta13
+            * (
+                _sinSqTheta12 * sin(_DeltaMSq32 * tmp) ** 2
+                + _cosSqTheta12 * sin(_DeltaMSq31 * tmp) ** 2
+            )
+            - sinSq2Theta12 * _cosQuTheta13 * sin(DeltaMSq21 * tmp) ** 2
         )
-        - sinSq2Theta12 * _cosQuTheta13 * sin(DeltaMSq21 * L4E[:]) ** 2
-    )
 
 
 class NueSurvivalProbability(FunctionNode):
@@ -71,9 +78,7 @@ class NueSurvivalProbability(FunctionNode):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._labels.setdefault(
-            "mark", "P(E, L, sin²2θ₁₂, sin²2θ₁₃, Δm²₂₁, Δm²₃₂, α)"
-        )
+        self._labels.setdefault("mark", "P(ee)")
         self.add_input(
             (
                 "L",
@@ -89,19 +94,6 @@ class NueSurvivalProbability(FunctionNode):
 
     def _typefunc(self) -> None:
         """A output takes this function to determine the dtype and shape"""
-        check_input_dimension(
-            self,
-            (
-                "E",
-                "L",
-                "sinSq2Theta12",
-                "sinSq2Theta13",
-                "DeltaMSq21",
-                "DeltaMSq32",
-                "alpha",
-            ),
-            1,
-        )
         check_input_shape(
             self,
             (
@@ -114,6 +106,7 @@ class NueSurvivalProbability(FunctionNode):
             ),
             (1,),
         )
+        check_input_subtype(self, "alpha", integer)
         copy_from_input_to_output(self, "E", "result")
 
     def _post_allocate(self):
@@ -121,8 +114,9 @@ class NueSurvivalProbability(FunctionNode):
         self.__buffer = empty(dtype=Edd.dtype, shape=Edd.shape)
 
     def _fcn(self, _, inputs, outputs):
-        out = outputs["result"].data
-        E = inputs["E"].data
+        buffer = self.__buffer.ravel()
+        out = outputs["result"].data.ravel()
+        E = inputs["E"].data.ravel()
         L = inputs["L"].data[0]
         sinSq2Theta12 = inputs["sinSq2Theta12"].data[0]
         sinSq2Theta13 = inputs["sinSq2Theta13"].data[0]
@@ -130,11 +124,11 @@ class NueSurvivalProbability(FunctionNode):
         DeltaMSq32 = inputs["DeltaMSq32"].data[0]
         alpha = inputs["alpha"].data[0]
 
-        self.__buffer[:] = L / 4.0 / E[:]  # common factor
-
         _osc_prob(
             out,
-            self.__buffer,
+            buffer,
+            E,
+            L,
             sinSq2Theta12,
             sinSq2Theta13,
             DeltaMSq21,
