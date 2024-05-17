d106b094f98737c58d9118f47c7d104a143314e6
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2024-05-17 15:38:55 +0300

    feat: add rescaling option for InverseSquareLaw

--- dgf_reactoranueosc/InverseSquareLaw.py
+++ dgf_reactoranueosc/InverseSquareLaw.py
@@ -1,7 +1,10 @@
-from dagflow.lib.OneToOneNode import OneToOneNode
+from typing import Literal
+
 from numba import njit
-from numpy import pi
+from numpy import multiply, pi
 from numpy.typing import NDArray
+
+from dagflow.lib.OneToOneNode import OneToOneNode
 
 _pi4 = 0.25 / pi
 
@@ -10,6 +13,13 @@ _pi4 = 0.25 / pi
 def _inv_sq_law(data: NDArray, out: NDArray):
     for i in range(len(out)):
         out[i] = _pi4 / data[i] ** 2
+
+
+_scales = {
+    "km_to_cm": 1e-10,
+    "m_to_cm": 1e-4,
+    None: None
+}
 
 
 class InverseSquareLaw(OneToOneNode):
@@ -23,10 +33,22 @@ class InverseSquareLaw(OneToOneNode):
     Calcultes an inverse-square law distribution
     """
 
-    def __init__(self, *args, **kwargs):
+    __slots__ = ("_scale",)
+    _scale: float | None
+
+    def __init__(
+        self,
+        *args,
+        scale: Literal["km_to_cm", "m_to_cm", None] = None,
+        **kwargs
+    ):
         super().__init__(*args, **kwargs)
         self._labels.setdefault("mark", "1/(4πL²)")
+        self._scale = _scales[scale]
 
     def _fcn(self):
         for inp, out in zip(self.inputs.iter_data(), self.outputs.iter_data()):
             _inv_sq_law(inp.ravel(), out.ravel())
+
+            if (scale:=self._scale) is not None:
+                multiply(out, scale, out=out)
