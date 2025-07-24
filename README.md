9fdde6cf1fa25b4cf7e4a36b2532800380f02b4a
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2025-07-24 16:09:26 +0300

    chore: add CHANGELOG, CONTRIBUTING, LICENSE, README and conftest.py

---
+++ README.md
@@ -0,0 +1,18 @@
+# Summary
+
+[![python](https://img.shields.io/badge/python-3.11-purple.svg)](https://www.python.org/)
+[![pipeline](https://git.jinr.ru/dag-computing/dagflow-reactorenueosc/badges/master/pipeline.svg)](https://git.jinr.ru/dag-computing/dagflow-reactorenueosc/commits/master)
+[![coverage report](https://git.jinr.ru/dag-computing/dagflow-reactorenueosc/badges/master/coverage.svg)](https://git.jinr.ru/dag-computing/dagflow-reactorenueosc/-/commits/master)
+
+<!--- Uncomment here after adding docs!
+[![pages](https://img.shields.io/badge/pages-link-white.svg)](http://dag-computing.pages.jinr.ru/dag-flow)
+-->
+
+The **dgm-reactor-neutrino** python package contains a set of calculation nodes to implement reactor electron antineutrino (oscillation) analysis in [DAGModelling](https://github.com/dagflow-team/dag-modelling) framework.
+
+## Repositories
+
+- Main repo:
+  - Development/CI: https://git.jinr.ru/dag-computing/dagflow-reactorenueosc
+  - Contact/pypi/mirror: https://github.com/dagflow-team/dgm-reactor-neutrino
+  - PYPI: https://test.pypi.org/project/dgm-reactor-neutrino
--- conftest.py
+++ conftest.py
@@ -1,18 +1,18 @@
-from os import chdir, getcwd, mkdir, listdir, environ
+from os import chdir, environ, getcwd, listdir, mkdir
 from os.path import isdir
 
 from pytest import fixture
 
 
 def pytest_sessionstart(session):
-    """
-    Called after the Session object has been created and
-    before performing collection and entering the run test loop.
+    """Called after the Session object has been created and before performing
+    collection and entering the run test loop.
 
-    Automatic change path to the `dag-flow/tests` and create `tests/output` dir
+    Automatic change path to the `dgm-reactor-neutrino/tests` and create
+    `tests/output` dir
     """
     while path := getcwd():
-        if (lastdir := path.split("/")[-1]) == "test":
+        if (lastdir := path.split("/")[-1]) == "tests":
             break
         elif ".git" in listdir(path):
             chdir("./tests")
@@ -24,7 +24,12 @@ def pytest_sessionstart(session):
 
 
 def pytest_addoption(parser):
-    parser.addoption("--debug-graph", action="store_true", default=False)
+    parser.addoption(
+        "--debug-graph",
+        action="store_true",
+        default=False,
+        help="set debug=True for all the graphs in tests",
+    )
 
 
 @fixture(scope="session")
@@ -34,7 +39,7 @@ def debug_graph(request):
 
 @fixture()
 def testname():
-    """Returns corrected full name of a test"""
+    """Returns corrected full name of a test."""
     name = environ.get("PYTEST_CURRENT_TEST").split(":")[-1].split(" ")[0]
     name = name.replace("[", "_").replace("]", "")
     return name
