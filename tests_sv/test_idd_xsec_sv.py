f50b1502adbe19bdffc2b6ff729ed4dd4a0f9634
Роман  Миннахметов <romanminnahmetov@MacBook-Air-Roman.local>
2025-08-02 19:49:58 +0300

    Add not working test

--- tests_sv/test_idd_xsec_sv.py
+++ tests_sv/test_idd_xsec_sv.py
@@ -1,14 +1,18 @@
-import numpy as np
-from dgm_reactor_neutrino.ibd_xsec_sv import IBDXsecSVO1
+from numpy import linspace, meshgrid
+from matplotlib.pyplot import subplots
 
+from dgm_reactor_neutrino.ibd_xsec_sv import IBDXsecSV
+from dgm_reactor_neutrino import EeToEnu, Jacobian_dEnu_dEe
+from dag_modelling.core.graph import Graph
+from dag_modelling.bundles.load_parameters import load_parameters
+from dag_modelling.lib.common import Array
+from dag_modelling.plot.plot import plot_auto
+from dag_modelling.plot.graphviz import savegraph
 
 
 
 def test_IBDXsecSV(debug_graph, test_name: str, output_path: str):
-    # enu = np.linspace(0, 12.0, 121, dtype="d")
-    # ctheta = np.linspace(-1, 1, 5, dtype="d")
-    # enu2, ctheta2 = np.meshgrid(enu, ctheta, indexing="ij")
-    # result = np.array([])
+
     data = {
         "format": "value",
         "state": "fixed",
@@ -25,9 +29,61 @@ def test_IBDXsecSV(debug_graph, test_name: str, output_path: str):
             "M_Z": 80.385 * 1.0e3
         },
         "labels":{
-
+            "NeutronMass": "neutron mass, MeV",
+            "ProtonMass": "proton mass, MeV",
+            "ElectronMass": "electron mass, MeV",
+            "PionMass": "pion mass, MeV",
+            "CosOfCab": "the cosine of Cabbibo`s angle",
+            "xi": "the difference between the proton and neutron anomalous magnetic moments in units of the nuclear magneton",
+            "g1_0": "axial-vector coupling constant g at param t equal zero",
+            "MAsq": "axial mass in square, MeV^2",
+            "MVsq": "vector mass in square, MeV^2",
+            "M_Z": "Z-boson mass, MeV"
         }
     }
+
+    enu1 = linspace(0, 12.0, 121)
+    ee1 = enu1.copy()
+    ctheta1 = linspace(-1, 1, 5)
+    enu2, ctheta2 = meshgrid(enu1, ctheta1, indexing="ij")
+    ee2, _ = meshgrid(ee1, ctheta1, indexing="ij")
+    with Graph(debug=debug_graph, close_on_exit=True) as graph:
+        storage = load_parameters(data)
+
+        enu = Array("enu", enu2, mode="fill")
+        ee = Array("ee", ee2, mode="fill")
+        ctheta = Array("ctheta", ctheta2, mode="fill")
+
+        ibdxsec_enu = IBDXsecSV("ibd_Eν")
+        ibdxsec_ee = IBDXsecSV("ibd_Ee")
+        eetoenu = EeToEnu("Enu")
+        jacobian = Jacobian_dEnu_dEe("dEν/dEe")
+
+        ibdxsec_enu << storage("parameters.constant")
+        ibdxsec_ee << storage("parameters.constant")
+        eetoenu << storage("parameters.constant")
+        jacobian << storage("parameters.constant")
+
+        (enu, ctheta) >> ibdxsec_enu
+        (ee, ctheta) >> eetoenu
+        (eetoenu, ee, ctheta) >> jacobian
+        (eetoenu, ctheta) >> ibdxsec_ee
+
+    subplots(1, 1)
+
+    plot_auto(
+        ibdxsec_enu,
+        plotoptions={"method": "pcolormesh"},
+        colorbar=True,
+        filter_kw={"masked_value": 0},
+        show=False,
+        close=True,
+        save=f"{output_path}/{test_name}_plot.pdf",
+    )
+
+    savegraph(graph, f"{output_path}/{test_name}.pdf")
+
+
 
 
 
